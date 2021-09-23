10e4ee83271f7c3afcc79c024fb040d00948f535
Raul Bob <bobraulfineas@gmail.com>
2021-09-23 16:28:10 +0300

    Added HATEOAS to delete favourit eaccounts.

--- src/main/java/hcl/hackathon/hcl/controller/CustomerController.java
+++ src/main/java/hcl/hackathon/hcl/controller/CustomerController.java
@@ -3,8 +3,12 @@ package hcl.hackathon.hcl.controller;
 import hcl.hackathon.hcl.controller.requests.AddFavouriteBankRequest;
 import hcl.hackathon.hcl.controller.requests.DeleteFavouriteBankRequest;
 import hcl.hackathon.hcl.controller.requests.EditFavouriteBankRequest;
+import hcl.hackathon.hcl.controller.response.ErrorResponse;
+import hcl.hackathon.hcl.controller.response.SuccessResponse;
 import hcl.hackathon.hcl.service.FavouriteBankAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,10 +37,16 @@ public class CustomerController {
     @DeleteMapping("/api/customers/favouriteaccounts")
     public ResponseEntity<?> deleteAccount(@RequestBody DeleteFavouriteBankRequest request) {
         if (request.getAccountId() == null || favouriteBankAccountService.findById(request.getAccountId()).isEmpty()) {
-            return new ResponseEntity<>("Account not found", HttpStatus.NOT_FOUND);
+            ErrorResponse errorResponse = new ErrorResponse("Account not found", null,  HttpStatus.NOT_FOUND.value());
+            EntityModel<ErrorResponse> resource = EntityModel.of(errorResponse);
+            resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).deleteAccount(request)).withSelfRel());
+            return new ResponseEntity<>(resource, HttpStatus.NOT_FOUND);
         } else {
             favouriteBankAccountService.deleteById(request.getAccountId());
-            return new ResponseEntity<>("Favorite Bank Account deleted", HttpStatus.ACCEPTED);
+            SuccessResponse successResponse = new SuccessResponse("Favorite Bank Account deleted", null, HttpStatus.ACCEPTED.value());
+            EntityModel<SuccessResponse> resource = EntityModel.of(successResponse);
+            resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).deleteAccount(request)).withSelfRel());
+            return new ResponseEntity<>(resource, HttpStatus.ACCEPTED);
         }
     }
 
--- src/main/java/hcl/hackathon/hcl/controller/requests/EditFavouriteBankRequest.java
+++ src/main/java/hcl/hackathon/hcl/controller/requests/EditFavouriteBankRequest.java
@@ -1,4 +1,18 @@
 package hcl.hackathon.hcl.controller.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EditFavouriteBankRequest {
+
+    private Long accountId;
+
+    private String newAccountName;
+
+    private String newAccountNumber;
+
 }
