65581dfd88cd90b941b7047ae041954cee677db8
AntoanRusinov <antoan.rusinov@gmail.com>
2021-09-23 13:24:58 +0300

    Deleted a class added by mistake

--- src/main/java/hcl/hackathon/hcl/repository/FavoriteBankAccount.java
+++
@@ -1,7 +0,0 @@
-package hcl.hackathon.hcl.repository;
-
-import hcl.hackathon.hcl.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteBankAccount extends JpaRepository<Customer, Long> {
-}
