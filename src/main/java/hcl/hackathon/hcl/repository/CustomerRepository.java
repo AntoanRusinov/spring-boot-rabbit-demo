8731e8f7a0f42fe45f277cd1cac507714a2ecc8e
AntoanRusinov <antoan.rusinov@gmail.com>
2021-09-23 13:38:24 +0300

    Removed not needed method

--- src/main/java/hcl/hackathon/hcl/repository/CustomerRepository.java
+++ src/main/java/hcl/hackathon/hcl/repository/CustomerRepository.java
@@ -2,10 +2,7 @@ package hcl.hackathon.hcl.repository;
 
 import hcl.hackathon.hcl.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long>  {
-
-    Optional<Customer> findByCustomerId(String customer_id);
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
