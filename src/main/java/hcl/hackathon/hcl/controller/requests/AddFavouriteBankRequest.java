d206edba7aca5398f4b9255135953d281fb86d9b
manish <kalpnadhangar@gmail.com>
2021-09-23 14:39:28 +0200

    RequestController

--- src/main/java/hcl/hackathon/hcl/controller/requests/AddFavouriteBankRequest.java
+++ src/main/java/hcl/hackathon/hcl/controller/requests/AddFavouriteBankRequest.java
@@ -1,4 +1,26 @@
 package hcl.hackathon.hcl.controller.requests;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
 public class AddFavouriteBankRequest {
+	
+    
+    @NotBlank(message = "'BankName' is mandatory")
+    private String name;
+    @NotBlank(message = "'Iban' is mandatory")
+    private String iban;
+
+
 }
+
