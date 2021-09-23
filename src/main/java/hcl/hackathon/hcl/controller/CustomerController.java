736cfbea416e2b0e6692a256cb669c5930ab5c33
Raul Bob <bobraulfineas@gmail.com>
2021-09-23 14:47:15 +0300

    Added Delete Account endpoint.

--- src/main/java/hcl/hackathon/hcl/controller/CustomerController.java
+++ src/main/java/hcl/hackathon/hcl/controller/CustomerController.java
@@ -3,14 +3,17 @@ package hcl.hackathon.hcl.controller;
 import hcl.hackathon.hcl.controller.requests.AddFavouriteBankRequest;
 import hcl.hackathon.hcl.controller.requests.DeleteFavouriteBankRequest;
 import hcl.hackathon.hcl.controller.requests.EditFavouriteBankRequest;
+import hcl.hackathon.hcl.service.FavouriteBankAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
+
+    @Autowired
+    private FavouriteBankAccountService favouriteBankAccountService;
 
     @PostMapping("/api/customers/login")
     public ResponseEntity<?> login() {
@@ -28,8 +31,13 @@ public class CustomerController {
     }
 
     @DeleteMapping("/api/customers/favouriteaccounts")
-    public ResponseEntity<?> addFavourites(DeleteFavouriteBankRequest request) {
-        return ResponseEntity.ok("null");
+    public ResponseEntity<?> deleteAccount(@RequestBody DeleteFavouriteBankRequest request) {
+        if (request.getAccountId() == null || favouriteBankAccountService.findById(request.getAccountId()).isEmpty()) {
+            return new ResponseEntity<>("Account not found", HttpStatus.NOT_FOUND);
+        } else {
+            favouriteBankAccountService.deleteById(request.getAccountId());
+            return new ResponseEntity<>("Favorite Bank Account deleted", HttpStatus.ACCEPTED);
+        }
     }
 
 }
--- src/main/java/hcl/hackathon/hcl/controller/requests/DeleteFavouriteBankRequest.java
+++ src/main/java/hcl/hackathon/hcl/controller/requests/DeleteFavouriteBankRequest.java
@@ -1,4 +1,14 @@
 package hcl.hackathon.hcl.controller.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DeleteFavouriteBankRequest {
+
+    private Long accountId;
+
 }
